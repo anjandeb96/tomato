eb28bc13ec988ec772ddae1a61cc1fa0d7e5306e
anjandeb96 <139786732+anjandeb96@users.noreply.github.com>
2023-07-29 13:32:26 +0600

    Update app.py

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ import tempfile
 
 @st.cache(allow_output_mutation=True)
 def load_model():
-  model=tf.keras.models.load_model('/content/drive/MyDrive/model_v2b2.h5')
+  model=tf.keras.models.load_model('model_v2b2.h5')
   return model
 with st.spinner('Model is being loaded..'):
   model=load_model()
